4bae85f2afdc28874411c030dc9b242a30c84ad7
MahmoudKhalid <mahmoud0122549@gmail.com>
2024-11-12 17:33:49 +0200

    chore: add instructor code

--- js/async/app.js
+++ js/async/app.js
@@ -1,7 +1,8 @@
+// MY SOLUCTION
 "use strict";
 const body = document.body;
 const image = document.createElement("img");
-body.insertAdjacentElement("afterbegin", image);
+// body.insertAdjacentElement("afterbegin", image);
 
 const displayImage = (path) => {
   return new Promise(function (resolve) {
@@ -30,6 +31,10 @@ const loadImages = () => {
     })
     .then(() => {
       showImage();
+      return wait(2);
+    })
+    .then(() => {
+      hideImage();
     });
 };
 
@@ -41,4 +46,43 @@ function wait(secs) {
   });
 }
 
-loadImages();
+// loadImages();
+
+// INSTRUCTOR SOLUCTION
+const imageContainer = document.body;
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = imgPath;
+    img.addEventListener("load", () => {
+      imageContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener("error", function () {
+      reject(new Error("Image not found"));
+    });
+  });
+};
+
+let curImage;
+createImage("./eagle_bird_predator_113991_1280x1024.jpg")
+  .then((img) => {
+    curImage = img;
+    console.log("image 1 loaded");
+    return wait(2);
+  })
+  .then(() => {
+    curImage.style.display = "none";
+    return createImage("./horses_sunset_paddock_125690_1280x1024.jpg");
+  })
+  .then((img) => {
+    curImage = img;
+    console.log("image 2 loaded");
+    return wait(2);
+  })
+  .then(() => {
+    curImage.style.display = "none";
+  })
+  .catch((err) => console.log(err.message));
